76687f1ae774cebbbe03b7010cffef9d494291f7
lenor4ik <leno4kamay08051987@gmail.com>
2023-11-24 15:51:30 +0100

    dz-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,15 @@
-'use strict';
-function getElementWidth(content, padding, border) { 
-return Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border)
-};
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+function filterArray(numbers, value) {
+    let arr = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            arr.push(numbers[i]);
+        }
+    }
+    return arr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
